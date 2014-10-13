305228f973f604202e683eceea27ddbbfe1b0f93
galicians <galicians@gmail.com>
2014-10-13 14:06:48 +0100

    docking station release bikes tested

--- src/dockingStation.js
+++ src/dockingStation.js
@@ -7,22 +7,12 @@ DockingStation.prototype.dockBike = function(bike) {
 	this.bikes.push(bike);
 }
 
-// DockingStation.prototype.releaseBike = function(bike) {
-// 	var bikeToRelease = [];
-// 	for (var index in this.bikes) {
-// 		if (this.bikes[index] == bike) {
-// 			bikeToRelease.push[bike];
-// 		}
-// 	}
-// 	this.bikes
+DockingStation.prototype.releaseBike = function(bike) {
+	var index = this.bikes.indexOf(bike);
+	return index != -1 ? removeBikeFromStation(this.bikes) : false;
+	function removeBikeFromStation(bikesInStation) {
+		bikesInStation.splice(index,1)
+		return bike;
+	}
+}
 
-// }
-
-// DockingStation.prototype.releaseBike = function(bike) {
-// 	var index = this.bikes.indexOf(bike);
-// 	index == -1 ? return false : removeBike
-// 	function removeBike(bike) {
-// 		this.bikes.slice(index, 1);
-// 		return bike;
-// 	} 
-// }
